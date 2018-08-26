201402f81f3be6ccbddbaa8957be9c0904e8483c
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2018-08-27 01:21:22 +0700

    just use file

--- back-end/index.js
+++ back-end/index.js
@@ -4,31 +4,8 @@ import formidable from 'formidable';
 import fs from 'fs';
 import _ from 'lodash';
 import bodyParser from 'body-parser';
-import MongoClient from 'mongodb';
-import mysql from 'mysql';
 
 import { convertDataToObject, checkText } from './helper';
-
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'mydb'
-});
-// const mysqlConnection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'mydb'
-// });
-
-// let wordsObj = {};
-// let wordsString = [];
-
-const mongodbConnection = MongoClient.MongoClient;
-// const ObjectID = MongoClient.ObjectID;
-const url = 'mongodb://localhost:27017/';
 
 const app = express();
 app.use(cors());
@@ -48,24 +25,14 @@ app.post('/upload', (req, res) => {
         if (err) throw err;
         // data will contain the file contents
         // blacklist words
-        const wordsObj = convertDataToObject(data);
-        // connect to db
-        mongodbConnection.connect(url, (err, client) => {
+        const addedWords = convertDataToObject(data);
+        fs.readFile('words.json', 'utf8', (err, data) => {
           if (err) throw err;
-          const db = client.db('mydb');
-          db.collection('words').findOne({}, (err, result) => {
-            if (err) throw err;
-            // update the list
-            const updated = _.merge(result, wordsObj);
-            db.collection('words').updateOne({ _id: result._id }, { $set: updated }, (err, response) => {
-              if (err) throw err;
-              client.close();
-              console.log('convert done', new Date());
-              res.json({ data: 'saved file' });
-            });
-          });
+          const updatedWords = _.merge(addedWords, JSON.parse(data));
+          fs.writeFile('words.json', JSON.stringify(updatedWords), 'utf8', () => false);
+          console.log('convert done', new Date());
+          res.json({ data: 'saved file' });
         });
-        // fs.writeFile('json.json', JSON.stringify(wordsObj), 'utf8', () => true);
       });
     } catch(e) {
       res.json({ data: 'error' });
@@ -77,17 +44,11 @@ app.post('/text',  (req, res) => {
   console.log('text receive', new Date());
   const text = req.body.data;
   try {
-    mongodbConnection.connect(url, (err, client) => {
+    fs.readFile('words.json', 'utf8', (err, data) => {
       if (err) throw err;
-      const db = client.db('mydb');
-      db.collection('words').findOne({}, (err, wordsObj) => {
-        if (err) throw err;
-        // update the list
-        const result = checkText(text, wordsObj);
-        console.log('done process', new Date());
-        client.close();
-        res.json({ data: result });
-      });
+      const result = checkText(text, JSON.parse(data));
+      console.log('process done', new Date());
+      res.json({ data: result });
     });
   } catch(e) {
     res.json({ data: 'error' });
@@ -95,31 +56,28 @@ app.post('/text',  (req, res) => {
 });
 
 app.post('/upload-split', (req, res) => {
-  console.log('receive', new Date());
+  console.log('receive-split', new Date());
   const form = new formidable.IncomingForm();
   form.parse(req);
   form.on('file', (name, file) => {
     try {
       fs.readFile(file.path, 'utf8', (err, data) => {
-        if (err) {
-          throw err;
-        };
-        const wordsString = data.split('\r\n');
-        // const insertArray = [];
-        wordsString.forEach(word => insertArray.push([word]));
-        pool.getConnection((err, connection) => {
+        if (err) throw err;
+        // data will contain the file contents
+        // blacklist words
+        const addedWords = data.split('\r\n');
+        const addedWordsSet = new Set([...addedWords]);
+        fs.readFile('split.txt', 'utf8', (err, data) => {
           if (err) throw err;
-          const sql = 'INSERT INTO words (word) VALUES ?';
-          connection.query(sql, [insertArray], (err, result) => {
-            connection.release();
-            if (err) throw err;
-            console.log('Number of records inserted: ' + result.affectedRows, new Date());
-            res.json({ data: 'saved file' });
-          });
+          const arrayData = data.split(',');
+          const arraySet = new Set(arrayData);
+          const updatedWordsSet = new Set([...arraySet, ...addedWordsSet])
+          fs.writeFile('split.txt', Array.from(updatedWordsSet), 'utf8', () => false);
+          console.log('convert done', new Date());
+          res.json({ data: 'saved file' });
         });
       });
     } catch(e) {
-      console.log('error', new Date());
       res.json({ data: 'error' });
     }
   });
@@ -129,24 +87,21 @@ app.post('/text-split',  (req, res) => {
   console.log('text receive', new Date());
   const text = req.body.data;
   try {
-    pool.getConnection((err, connection) => {
+    fs.readFile('split.txt', 'utf8', (err, data) => {
       if (err) throw err;
-      connection.query('SELECT * FROM words', (err, result, fields) => {
-        connection.release();
-        if (err) throw err;
-        const check = text => {
-          for (let i = 0; i < result.length; i++) {
-            if (result[i].word && text.includes(result[i].word.trim())) {
-              // console.log(`s${wordsString[i]}s`);
-              return true
-            }
+      const arrayData = data.split(',');
+      const check = text => {
+        for (let i = 0; i < arrayData.length; i++) {
+          if (arrayData[i] && text.includes(arrayData[i].trim())) {
+            console.log(`=== ${arrayData[i]} ===`);
+            return true
           }
-          return false;
         }
-        const isContain = check(text);
-        console.log('done process', new Date());
-        res.json({ data: isContain });
-      });
+        return false;
+      }
+      const isContain = check(text);
+      console.log('done process', new Date());
+      res.json({ data: isContain });
     });
   } catch(e) {
     console.log('error', new Date());
